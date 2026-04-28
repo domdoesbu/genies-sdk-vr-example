e17b05ee79baf5decc1609138e068da8c94bc999
Dom Doesburg <94422248+domdoesbu@users.noreply.github.com>
2026-04-28 12:14:40 -0400

    Pick up and put in basket work
    Logic for pick up and basket work. Tested on one bundt cake without NPC interactions, and player loses a heart.

---
+++ Assets/Scripts/Desktop_Scripts/Player.cs
@@ -0,0 +1,88 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+//https://www.youtube.com/watch?v=pzaxC-P3sgs
+public class Player : MonoBehaviour
+{
+    [SerializeField] private LayerMask pickableLayerMask;
+    [SerializeField] private LayerMask basketLayerMask;
+    [SerializeField] private Transform playerCameraTransform;
+    [SerializeField] private GameObject pickUpUI;
+    [SerializeField][Min(1)] private float hitRange = 3;
+    private RaycastHit hit;
+    [SerializeField] private InputActionReference interactionInput, dropInput;
+    public Transform avatarHand;
+    [SerializeField] private GameObject inHandItem;
+    public GameObject basket;
+    private void Start()
+    {
+        interactionInput.action.performed += Interact;
+    }
+    private void Update()
+    {
+        Debug.DrawRay(playerCameraTransform.position, playerCameraTransform.forward * hitRange, Color.red);
+
+        
+        if(hit.collider != null)
+        {
+            Debug.Log(hit.collider.name);
+            hit.collider.GetComponent<Highlight>()?.ToggleHighlight(false);
+            //pickUpUI.SetActive(false);
+        }
+        
+        // If item in hand, don't detect anything else
+        if (inHandItem != null )
+        {
+            
+            if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out hit, hitRange, basketLayerMask))
+            {
+                Debug.Log("basket");
+            }
+            return;
+        }
+
+        if(Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out hit, hitRange, pickableLayerMask))
+        {
+            hit.collider.GetComponent<Highlight>()?.ToggleHighlight(true);
+            //pickUpUI.SetActive(true);
+        }
+    }
+
+    public void FindAvatarHand()
+    {
+        avatarHand = GameObject.Find("RightHandBind").transform;
+    }
+
+    private void Interact(InputAction.CallbackContext obj)
+    {
+        Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+        if (hit.collider != null && inHandItem == null && hit.collider.GetComponent<Basket>() == null)
+        {
+
+            Debug.Log(hit.collider.name);
+            inHandItem = hit.collider.gameObject;
+            inHandItem.transform.SetParent(avatarHand.transform, false);
+            inHandItem.transform.localPosition = Vector3.zero;
+            inHandItem.transform.rotation = Quaternion.identity;
+
+            if (rb != null)
+            {
+                rb.isKinematic = true;
+            }
+            return;
+        }
+        else if (hit.collider != null && inHandItem != null && hit.collider.GetComponent<Basket>() != null)
+        {
+            Debug.Log("Put in basket");
+            
+            inHandItem.transform.SetParent(hit.collider.transform, false);
+            inHandItem.transform.localPosition = Vector3.zero;
+            inHandItem.transform.rotation = Quaternion.identity;
+            if (rb != null)
+            {
+                rb.isKinematic = true;
+            }
+        }
+    }
+}
